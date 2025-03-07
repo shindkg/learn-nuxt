15aa2690dc4e75e63d63acf935824f434231ef57
shindkg <shindkg@gmail.com>
2025-03-07 15:50:06 +0900

    섹션 2 - Nuxt.js 프로젝트 시작하기

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- route -->
+    <h1>{{ $route.name }} (default.vue 라우트) 페이지</h1>
+    <!-- NuxtLink == router-link -->
+    <NuxtLink to="/">홈페이지</NuxtLink>
+    <NuxtLink to="/main">메인페이지</NuxtLink>
+    <NuxtLink to="/product">상품페이지</NuxtLink>
+    <!-- Nuxt == router-view -->
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log(this.$route)
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template lang="">
+  <div>
+      <p>넉스트 시작하기 - 인덱스 페이지(index.vue)</p>
+  </div>
+</template>
+<script>
+export default {  
+}
+</script>
+<style lang="">
+  
+</style>
